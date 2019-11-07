880aecf4c9fba28fa6ae9ea01b065c29841b15a5
Milind Oka <oak444@gmail.com>
2019-11-07 20:41:34 +0530

    New Revised PDF creation started

--- app/src/main/java/in/org/ilugbom/attendance/MonthlyReport.java
+++ app/src/main/java/in/org/ilugbom/attendance/MonthlyReport.java
@@ -10,6 +10,7 @@ import android.widget.GridView;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
@@ -26,6 +27,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.StringReader;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import javax.security.auth.Subject;
@@ -82,8 +84,8 @@ public class MonthlyReport {
         document.open();
 //            document.add(new Paragraph("Hello World!"));
         AddHeader(document);
-        FillHeaderMmatrix();
-        FillMatrix();
+         FillHeaderMmatrix();
+    //    FillMatrix();
         matrixtoPdf(document);
 
         document.close();
@@ -147,10 +149,10 @@ public class MonthlyReport {
             String AttendenceData;
 
             int Last= PresencyLine.size();
-            rollRange = RollNos.get(0);
-            String RollNoArray [] = rollRange.split(",");
+           // rollRange = RollNos.get(0);
+            String RollNoArray [] = RollNos.get(0).split(",");
          //   rollNoStart = RollNoArray[0];
-     
+
             int TotNoStnts = RollNoArray.length;
 
             for (int i = 0; i < TotNoStnts; i++) {         // matrix.length = Tot No of Students
@@ -260,6 +262,7 @@ public class MonthlyReport {
                     DateArray.add(temp[0]);
                     Divisions.add(temp[1]);
                     RollNos.add(temp[2]);
+                    Msg.Show(temp[2]);
                     PresencyLine.add(temp[3]);
                 }
             }
@@ -269,5 +272,265 @@ public class MonthlyReport {
             //    Toast.makeText(getBaseContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
         }
     }
+//////////////////// NEW ROUTINE FOR FLEXIBLE ROLL ////////////////////////
 
-}
+    int totalDays=0;
+    int PresentCount[];
+    String ClassDiv="";
+    ArrayList<String> roll=new ArrayList<String>();//creating new generic arraylist
+    ArrayList<String> attendanceLines=new ArrayList<String>();//creating new generic arraylist
+    ArrayList<String> APchain=new ArrayList<String>();//creating new generic arraylist
+    int strength=35,requiredtables=3;
+
+
+    Font normal = new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.NORMAL);
+
+
+    void FillRollArray()
+    {   roll.removeAll(roll);
+        APchain.removeAll(APchain);
+
+        String temp[],rolltemp[];
+
+        //assert 4 parts in first split
+        temp=attendanceLines.get(0).split("#");
+        ClassDiv=new String(temp[1]);
+        rolltemp=temp[2].split(",");
+
+
+        for (int x=0;x<rolltemp.length;x++)
+        { roll.add(rolltemp[x]);
+            APchain.add("");
+        }
+
+        PresentCount=new int[rolltemp.length];
+
+    }
+
+
+    void LoadAttendanceLines(String division, String month)
+    {
+        attendanceLines.removeAll(attendanceLines);
+        attendanceLines.add("02/11/19 - Sat - 11:53#IX-A#5,25,197,331,565,1012#APPPAP");
+        attendanceLines.add("03/11/19 - Sun - 20:23#IX-A#5,25,197,331,565,1012#PPAAAP");
+        attendanceLines.add("08/11/19 - Sat - 11:53#IX-A#5,25,197,331,565,1012#APPPAP");
+        attendanceLines.add("17/11/19 - Sat - 11:53#IX-A#5,25,197,331,565,1012#APPPAP");
+        attendanceLines.add("22/11/19 - Sat - 11:53#IX-A#5,25,197,331,565,1012#APPPAP");
+        attendanceLines.add("28/11/19 - Sat - 11:53#IX-A#5,25,197,331,565,1012#APPPAP");
+        attendanceLines.add("31/11/19 - Sat - 11:53#IX-A#5,25,197,331,565,1012#APPPAP");
+    }
+
+
+    void Check_ThirtyOneDays_And_Fill_APChain()
+    { boolean datefound=false;
+        String currentline="";
+        String lastblock="";
+        totalDays=0;
+        for(int i=1;i<32;i++)
+        {
+            String TwoDigitMonthDay=String.format("%02d",i);
+
+            datefound=false;
+            for(int j=0;j<attendanceLines.size();j++)
+            {   currentline=attendanceLines.get(j);
+                String str=attendanceLines.get(j).substring(0, 2);
+                if(str.equalsIgnoreCase(TwoDigitMonthDay))
+                {datefound=true; break;}
+            }
+
+            if(datefound)
+            { totalDays++;
+                lastblock="";
+                String temp[];
+                //assert 4 parts in first split
+                temp=currentline.split("#");
+                lastblock+=temp[3];
+
+            }
+
+
+
+            for(int k=0;k<roll.size();k++)
+            {
+
+                String temp2=APchain.get(k);
+
+                if(datefound)
+                { char ap=lastblock.charAt(k);
+                    if(ap=='P') PresentCount[k]++;
+                    APchain.set(k,temp2+ap);
+
+                }
+                else
+                    APchain.set(k,temp2+" ");
+
+            }
+        }
+
+    }
+
+
+    void PrintAttendanceReportPDF() throws DocumentException, IOException
+    {  LoadAttendanceLines(" ", " ");
+
+        if(strength>200) return;
+        requiredtables=strength/35;
+        if(strength%35!=0) requiredtables++;
+
+        String filename="monthlyAttendance.pdf";
+        Document document = new Document(PageSize.A4.rotate());
+        document.setMargins(50, 10, 25, 25);
+        PdfWriter.getInstance(document, new FileOutputStream(filename));
+        document.open();
+
+        //    com.itextpdf.text.Font NORMAL = new com.itextpdf.text.Font(FontFamily.TIMES_ROMAN, 12);
+
+        FillRollArray();
+        Check_ThirtyOneDays_And_Fill_APChain();
+
+        AddHeader(document);
+
+        AttendanceGrid(document);
+
+//	        AddFooter(document);
+
+
+        document.close();
+    }
+
+
+
+    void AddTheHeader(Document document) throws DocumentException, IOException
+    {
+        PdfPTable table = new PdfPTable(3);
+        PdfPCell cell = new PdfPCell(new Phrase(" "));
+        cell.setBorder(PdfPCell.NO_BORDER);
+        table.setWidthPercentage(95);
+        table.addCell(cell);
+
+
+        cell = new PdfPCell(new Phrase("SIWS College"));cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        table.addCell(cell);
+
+
+        cell = new PdfPCell(new Phrase(" "));cell.setBorder(PdfPCell.NO_BORDER);
+        table.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Name of Teacher :"));cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_LEFT);
+        table.addCell(cell);
+
+        System.out.println(ClassDiv);
+        cell = new PdfPCell(new Phrase("Class-Div : "+ ClassDiv));cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        table.addCell(cell);
+
+
+        cell = new PdfPCell(new Phrase("Subject :"));cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);cell.setBorder(PdfPCell.NO_BORDER);
+        table.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Date : 23/02/18"));cell.setBorder(PdfPCell.NO_BORDER);
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+        table.addCell(cell);
+
+        table.setSpacingAfter(10f);
+        document.add(table);
+
+
+    }
+
+    void AttendanceGrid(Document document) throws DocumentException, IOException
+    {
+
+        float col[]={4,8,2,2,2,2,2,2,2,2,2,2,
+                2,2,2,2,2,2,2,2,2,2,
+                2,2,2,2,2,2,2,2,2,2,
+                2,4,5};
+
+        //////////   TITLE ROW  ///////////////////////
+
+        PdfPTable table2 = new PdfPTable(col);
+        table2.setWidthPercentage(95);
+        PdfPCell cell = new PdfPCell(new Phrase("Sr No",normal));
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        cell.setPaddingBottom(5f);
+        table2.addCell(cell);
+
+        cell = new PdfPCell(new Phrase("Roll No",normal));
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        //cell.setBorder(PdfPCell.NO_BORDER);
+        table2.addCell(cell);
+
+        String dateno;
+        for(int j=0;j<31;j++)
+        {
+            dateno=String.format("%d",j+1);
+            cell = new PdfPCell(new Phrase(dateno,normal));
+            cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+            cell.setPaddingBottom(5f);
+            table2.addCell(cell);
+        }
+
+        cell = new PdfPCell(new Phrase("Total",normal));
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        //cell.setBorder(PdfPCell.NO_BORDER);
+        table2.addCell(cell);
+
+        cell = new PdfPCell(new Phrase(" % ",normal));
+        cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+        //cell.setBorder(PdfPCell.NO_BORDER);
+        table2.addCell(cell);
+
+        ////////////////// END OF TITLE ROW
+
+        ////////////// REMAING TABLE ROWS
+
+        String srno;
+        for (int i=0;i<roll.size();i++)
+        {
+            srno=String.format("%d",i+1);
+            cell = new PdfPCell(new Phrase(srno,normal));
+            cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+            cell.setPaddingBottom(5f);
+            table2.addCell(cell);
+
+            cell = new PdfPCell(new Phrase(roll.get(i),normal));
+            cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+            //cell.setBorder(PdfPCell.NO_BORDER);
+            table2.addCell(cell);
+
+            for(int j=0;j<31;j++)
+            { String APmark=""+APchain.get(i).charAt(j);
+                cell = new PdfPCell(new Phrase(APmark,normal));
+                cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+                cell.setPaddingBottom(5f);
+                table2.addCell(cell);
+            }
+
+            cell = new PdfPCell(new Phrase(String.format("%02d",PresentCount[i]),normal));
+            cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+            //cell.setBorder(PdfPCell.NO_BORDER);
+            table2.addCell(cell);
+
+
+            DecimalFormat df = new DecimalFormat("000.00");
+            double percent = PresentCount[i]*100/totalDays;
+            String percentage = df.format(percent);
+
+
+            cell = new PdfPCell(new Phrase(percentage,normal));
+            cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+            //cell.setBorder(PdfPCell.NO_BORDER);
+            table2.addCell(cell);
+
+        }
+
+        document.add(table2);
+    }
+
+
+}  ////class ends
+
+
